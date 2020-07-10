eb100aea83fc248d1cf6c29c854311c24d72b740
Caio Tarifa <caiotarifa@gmail.com>
2020-07-10 09:05:16 -0300

    Typography, minor changes and v0.0.4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const resolve = (...paths) => paths.map(path => sourcePath + path)
 
 const extendQuasar = quasar => {
   quasar.css.push(...resolve(
-    'css/spaces.scss'
+    'css/spaces.scss',
+    'css/typography.scss'
   ))
 }
 
